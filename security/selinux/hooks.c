bfa87c69745d02ffa072f3c310d670fdff64ea3a
Atakan <ata.kaner03@gmail.com>
2025-04-23 11:20:59 +0300

    selinux: Add toggleable permissive mode
    Change-Id: I4d35b0ff36d67d99089c1d0fea1291d2d2bce079

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -108,6 +108,7 @@ struct selinux_state selinux_state;
 /* SECMARK reference count */
 static atomic_t selinux_secmark_refcount = ATOMIC_INIT(0);
 
+#ifndef CONFIG_SECURITY_SELINUX_SET_PERMISSIVE
 #ifdef CONFIG_SECURITY_SELINUX_DEVELOP
 static int selinux_enforcing_boot __initdata;
 
@@ -121,6 +122,9 @@ static int __init enforcing_setup(char *str)
 __setup("enforcing=", enforcing_setup);
 #else
 #define selinux_enforcing_boot 1
+#endif
+#else
+#define selinux_enforcing_boot 0
 #endif
 
 int selinux_enabled_boot __initdata = 1;
