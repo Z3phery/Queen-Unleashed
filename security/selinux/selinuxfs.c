aff0d0dd634d899adf0882c169000433c7d04728
Atakan <ata.kaner03@gmail.com>
2025-09-12 19:12:13 +0300

    Revert "selinux: Add toggleable permissive mode"
    This reverts commit bfa87c69745d02ffa072f3c310d670fdff64ea3a.

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -137,7 +137,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 				 size_t count, loff_t *ppos)
 
 {
-#ifndef CONFIG_SECURITY_SELINUX_SET_PERMISSIVE
 	struct selinux_fs_info *fsi = file_inode(file)->i_sb->s_fs_info;
 	struct selinux_state *state = fsi->state;
 	char *page = NULL;
@@ -187,9 +186,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 out:
 	kfree(page);
 	return length;
-#else
-	return count;
-#endif
 }
 #else
 #define sel_write_enforce NULL
