97b3418b9970c35d9fa31ee87ae96bd1dc73bf2d
Z3phery <dz.ignacioaguilar@gmail.com>
2026-01-22 08:01:46 -0300

    Revert "cpuidle: Reject idle entry if need_resched() is true"
    This reverts commit 6b208b3198ced2742726ac8cad6847832329233d.

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -269,9 +269,7 @@ static inline int cpuidle_register_governor(struct cpuidle_governor *gov)
 ({									\
 	int __ret = 0;							\
 									\
-	if (need_resched()) {						\
-		__ret = -1;						\
-	} else if (!idx) {						\
+	if (!idx) {							\
 		cpu_do_idle();						\
 		return idx;						\
 	}								\
